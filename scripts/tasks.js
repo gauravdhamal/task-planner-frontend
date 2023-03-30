ad5f70ed920b2eb2892efca8c0486ea6d898fa4f
gauravdhamal <gauravdhamal11@gmail.com>
2023-03-30 21:02:02 +0530

    Updating URL

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -3,7 +3,8 @@ import navbar from "../components/navbar.js";
 document.querySelector("#navbar").innerHTML = navbar();
 
 // https://task-planner-backend-production.up.railway.app
-const commonUrl = "http://localhost:8080";
+// http://localhost:8080
+const commonUrl = "https://task-planner-backend-production.up.railway.app";
 
 let taskFormButtonOpen = document.getElementById("taskFormButtonOpen");
 let taskFormButtonClose = document.getElementById("taskFormButtonClose");
